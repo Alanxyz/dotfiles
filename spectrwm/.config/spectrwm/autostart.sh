597f50af447e05ce7ea8ce3c256444c7f84eaed1
AstralCam <AstralCam@protonmail.com>
2022-02-11 15:11:06 -0600

    barra de estado mejorada

--- spectrwm/.config/spectrwm/autostart.sh
+++ spectrwm/.config/spectrwm/autostart.sh
@@ -3,3 +3,4 @@
 setxkbmap us &
 xwallpaper --stretch ~/.bg.png &
 redshift -l 21.0161:-101.8671 &
+syncthing -no-browser &
--- spectrwm/.config/spectrwm/baraction.sh
+++ spectrwm/.config/spectrwm/baraction.sh
@@ -1,60 +1,66 @@
 #!/bin/bash
- 
+
 vol() {
     VOLONOFF="$(amixer -D pulse get Master | grep Left: | sed 's/[][]//g' | awk '{print $6}')"
     volu="$(amixer -D pulse get Master | awk -F'[][]' 'END{ print $2 }')"
- 
-MuteIcon="!V"
-VolIcon="V"
- 
-if [ "$VOLONOFF" = "on" ]; then
-    echo "$volu"
-else
-    echo "$MuteIcon Mute"
-fi
- 
+
+    MuteIcon="!V"
+    VolIcon="V"
+
+    if [ "$VOLONOFF" = "on" ]; then
+        echo "$volu"
+    else
+        echo "$MuteIcon Mute"
+    fi
+
 }
- 
+
 temp() {
-        temp="$(sensors | awk '/Core 0/ {print $3+0}')"  
+    temp="$(sensors | awk '/Core 0/ {print $3+0}')"  
     echo -e "$temp"℃""
 }
- 
+
 bat() {
-batstat="$(cat /sys/class/power_supply/BAT0/status)"
-battery="$(cat /sys/class/power_supply/BAT0/capacity)"
+    batstat="$(cat /sys/class/power_supply/BAT0/status)"
+    battery="$(cat /sys/class/power_supply/BAT0/capacity)"
     if [ $batstat = 'Unknown' ]; then
-    batstat="B"
+        batstat="B"
     elif [[ $battery -ge 5 ]] && [[ $battery -le 19 ]]; then
-    batstat="B"
+        batstat="B"
     elif [[ $battery -ge 20 ]] && [[ $battery -le 39 ]]; then
-    batstat="B"
+        batstat="B"
     elif [[ $battery -ge 40 ]] && [[ $battery -le 59 ]]; then
-    batstat="B"
+        batstat="B"
     elif [[ $battery -ge 60 ]] && [[ $battery -le 79 ]]; then
-    batstat="B"
+        batstat="B"
     elif [[ $battery -ge 80 ]] && [[ $battery -le 95 ]]; then
-    batstat="B"
+        batstat="B"
     elif [[ $battery -ge 96 ]] && [[ $battery -le 100 ]]; then
-    batstat="B"
-fi
- 
-echo "$battery"%""
+        batstat="B"
+    fi
+
+    echo "$battery"%""
 }
 
 wifi() {
-	echo $(nmcli -t -f name connection show --active)
+    echo $(nmcli -t -f name connection show --active)
 }
- 
-clock() {
 
-timedate=$(date +'%I:%M')
-	echo "$timedate" 
- 
+clock() {
+    timedate=$(date +'%I:%M')
+    echo "$timedate" 
 }
- 
-      SLEEP_SEC=60
-      while :; do     
-				echo   "$(wifi) | $(temp) | $(vol) | $(bat) "
-        sleep $SLEEP_SEC
-        done
+
+watter() {
+    curl wttr.in/?format=4
+}
+
+todo() {
+    ~/.local/bin/nt n
+}
+
+SLEEP_SEC=60
+while :; do     
+    echo  "$(todo) | $(wifi) | $(vol) | $(temp) | $(bat) "
+    sleep $SLEEP_SEC
+done
