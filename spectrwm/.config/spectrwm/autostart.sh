ab4f27aca6d47010e6579e134f15bfb2b03afaba
AstralCam <AstralCam@protonmail.com>
2023-10-07 23:48:50 -0600

    cambios generales y modo gruvbox

--- spectrwm/.config/spectrwm/autostart.sh
+++ spectrwm/.config/spectrwm/autostart.sh
@@ -4,8 +4,8 @@ setxkbmap us &
 setxkbmap -option caps:escape &
 
 # xwallpaper --stretch ~/.bg.png &
-hsetroot -solid "#303030" &
-picom &
+hsetroot -solid "#000000" &
+# picom &
 
 redshift &
 syncthing -no-browser &
